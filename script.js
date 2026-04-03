8610d7f37ced88a750f7483defc7c3c220fe37cb
atommy4152-spec <atommy4152@gmail.com>
2026-04-03 19:11:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("btn").addEventListener("click", function () {
-  alert("JavaScript 已經成功連上！");
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+
+yesBtn.addEventListener("click", function () {
+  message.textContent = "Yayyy! 💖";
+});
+
+noBtn.addEventListener("click", function () {
+  message.textContent = "再想一下嘛 🥺";
 });
